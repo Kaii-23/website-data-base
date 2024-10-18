8ffb964ba068123885abddbcb84c306d3377151e
S2301364 <S2301364@NDC03211>
2024-10-18 12:59:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using website2.Components;
+using website2.Models;
+using website2.Services;
+using MudBlazor.Services;
+
+
+namespace website2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddMudServices();
+
+
+            builder.Services.AddDbContext<TlS2301364RzaContext>(options =>
+                options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
+                new MySqlServerVersion(new Version(8, 0, 29))));
+
+            builder.Services.AddScoped<CustomerService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
---
+++ Services/CustomerService.cs
@@ -0,0 +1,24 @@
+﻿using website2.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace website2.Services
+{
+    public class CustomerService
+    {
+        private readonly TlS2301364RzaContext _context;
+
+        public CustomerService(TlS2301364RzaContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task AddCustomerAsync(Customer customer) 
+        {
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
